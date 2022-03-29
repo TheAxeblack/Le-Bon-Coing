c45c67ee9240d7369e8c527224b3e2593219454e
TheAxeblack <mathis.lecuyer.pro@gmail.com>
2022-03-29 07:40:19 +0200

    add main.php

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Site de petites annonces</title>
+    <!--A COMPLETER-->
+</head>
+<body>
+    <!--A COMPLETER-->
+</body>
+</html>
