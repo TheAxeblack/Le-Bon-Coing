f35e7b59fee2fa34be6a1580e9642ee5a053cb49
Mathis Lécuyer <mathis.lecuyer.pro@gmail.com>
2022-04-12 15:12:57 +0200

    Suppression boostrap

--- js/mesfonctions.js
+++ js/mesfonctions.js
@@ -1,19 +1,44 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function deroulerMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
+/* Lors de l'appui sur le button révele le menu */
+function deroulerMenu(identifiant) {
+    document.getElementById(identifiant).classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+/* Ferme le menu déroulant approprié si on clique en dehors */
 window.onclick = function (e) {
     if (!e.target.matches('.dropbtn')) {
         var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
+        var myDropdown2 = document.getElementById("myDropdown2");
+        var myDropdown3 = document.getElementById("myDropdown3");
+        if (myDropdown.classList.contains('show'))
             myDropdown.classList.remove('show');
-        }
+        else if (myDropdown2.classList.contains('show'))
+            myDropdown2.classList.remove('show');
+        else if (myDropdown3.classList.contains('show'))
+            myDropdown3.classList.remove(('show'));
     }
 }
 
 function choisirLangue() {
     alert('Coucou');
+}
+
+function changerMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+
+window.onscroll = function () {
+    fixerBarre()
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function fixerBarre() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
