d91c37ccd2e9acd3a974d9cdaa528852bd0615ca
bonsoirlina <103634051+bonsoirlina@users.noreply.github.com>
2022-04-14 16:09:31 +0200

    Base de Données

---
+++ connex.inc.php
@@ -0,0 +1,15 @@
+<?php
+function connexion($base)
+{
+    try {
+        $pdo = new PDO('sqlite:'.$base);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo 'Problème à la connexion';
+        die();
+    }
+
+    return $pdo;
+}
+
+?>
