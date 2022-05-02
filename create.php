f83c7d802cd856620b4ef68fd3c146b029276ffa
bonsoirlina <103634051+bonsoirlina@users.noreply.github.com>
2022-05-02 18:48:06 +0200

    DERNIERE VERSION

--- create.php
+++ create.php
@@ -44,5 +44,30 @@ $ann = "CREATE TABLE IF NOT EXISTS annonce_p(
 
 $pdo->exec($ann);
 
-      
+$mdp=md5('genshin42');
+$stmt=$pdo->prepare("INSERT INTO user (genre, prenom, nom, date_n, pseudo, mdp, email, statut) VALUES ('n-b', 'SuperUser', 'Admin', '2000-01-01', 'SuperUser', :mdp, 'mathislecuyer@gmail.com', 1);");
+$stmt->bindParam(':mdp', $mdp);
+$stmt->execute();
+
+$mdp3=md5('genshin75');
+$stmt3=$pdo->prepare("INSERT INTO user (genre, prenom, nom, date_n, pseudo, mdp, email) VALUES ('femme', 'Lina', 'Benali', '2002-10-10', 'bonsoirlina', :mdp3, 'lina.benali68@gmail.com');");
+$stmt3->bindParam(':mdp3', $mdp3);
+$stmt3->execute();
+
+
+
+$stmt4=$pdo->prepare("INSERT INTO annonce_p (id_u, nom, type, date_post, image1, image2, image3, description, prix, c_postal) VALUES ('2', 'Voiture peugeot 206+', 'vehicule', DATE(), 'images/peugeot-206-25.jpg', NULL, NULL, 'voiture à vendre, me contacter par mail pour plus de renseignement.', '1500', '42100');");
+$stmt4->execute();
+$stmt4->closeCursor();
+
+$stmt5=$pdo->prepare("INSERT INTO annonce_p (id_u, nom, type, date_post, image1, image2, image3, description, prix, c_postal) VALUES ('2', 'Collection manga demon slayer', 'multimedia', DATE(), 'images/demon-slayer-kimetsu-no-yaiba-wallpaper-04.jpg', NULL, NULL, 'collection de manga que je ne lis plus', '340', '42000');");
+$stmt5->execute();
+
+
+
+
+
+
+
+
 
